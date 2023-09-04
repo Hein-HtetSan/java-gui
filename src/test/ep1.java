d228e4d55373d886915a8f70fab4259f04db42fb
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-09-04 17:28:06 +0630

    testing JFrame

---
+++ src/test/ep1.java
@@ -0,0 +1,18 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+// testing JFrame as a subclass by extending it
+
+public class ep1 extends JFrame{
+    public ep1(){
+        setTitle("First GUI Program");
+        setSize(300, 200);
+        setLocation(150, 250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String args[]){
+        ep1 jf = new ep1();
+        jf.setVisible(true);
+    }
+}

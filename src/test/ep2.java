85bc3f7a6ea24f6167071b987a8af2d421d33580
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-09-04 17:28:21 +0630

    testing Content Pane

---
+++ src/test/ep2.java
@@ -0,0 +1,26 @@
+
+// testing Content Pane of a Frame
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ep2 extends JFrame {
+    public ep2(){
+        setTitle("Content Pane"); // set the window title
+        setSize(500, 500); // frame size of window
+        setLocation(100, 150); // position of frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // terminate program when click the close btn
+    }
+    public static void main(String args[]){
+        ep2 jf = new ep2(); // create instance of ep2
+        Container container = jf.getContentPane(); // content pane
+        container.setBackground(Color.BLUE); // set the content pane bg to blue
+
+        JButton okButton = new JButton("Ok"); // create button obj
+        container.setLayout(null); // i think this is for absolute positioning
+        okButton.setBounds(70, 125, 80, 30); // customize as you wish
+        container.add(okButton); // set the button onto content pane
+
+        jf.setVisible(true); // set visibility to true
+    }
+}
